0ccb1eef6770d40313b9766955ec0336962e6fd4
Ron Belmarch <belmarch+github@gmail.com>
2016-05-10 13:42:43 -0700

    added test text

--- nfc/ViewController.swift
+++ nfc/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("hello world")
     }
 
     override func didReceiveMemoryWarning() {
