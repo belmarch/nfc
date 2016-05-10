346d6dc77685bb33f958ea4a380ab11e9d7b0826
Ron Belmarch <belmarch+github@gmail.com>
2016-05-10 13:51:06 -0700

    First commit

--- nfc/ViewController.swift
+++ nfc/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hello world")
+        print("hello worlds")
     }
 
     override func didReceiveMemoryWarning() {
